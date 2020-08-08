86c859f4a9ddb58a291bf03a99b3f03b1f9d7428
makstakeda <mesketches@gmail.com>
2020-08-08 11:58:55 +0100

    createInitialState - tested

---
+++ src/modules/__tests__/create_initial_state.js
@@ -0,0 +1,32 @@
+/*global ymaps*/
+
+import { createInitialState, localeEn } from '../create_initial_state';
+
+describe('createInitialState', () => {
+  const apiKey = 'api-key';
+
+  beforeEach(() => {
+    const API_TIMEOUT = 500;
+
+    setTimeout(() => {
+      window.ymaps = { Map: {} };
+    }, API_TIMEOUT)
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('creates initial state for further map processing', async () => {
+    const config = await createInitialState({ apiKey, selectedLocale: 'en' });
+    expect(config).toEqual({ locale: localeEn });
+    expect(window.ymaps.Map).toBeDefined();
+
+    const apiScript = document.getElementById('api-maps.yandex');
+    expect(apiScript.src).toBe(`https://api-maps.yandex.ru/2.1/?lang=en&apikey=${apiKey}`);
+  });
+
+  it('fails to create initial state if locale is unsupported', () => {
+    expect(() => createInitialState({ apiKey, selectedLocale: 'jp' })).toThrowError(new SyntaxError(localeEn.langFail));
+  });
+});
--- src/modules/create_initial_state.js
+++ src/modules/create_initial_state.js
@@ -1,41 +1,31 @@
 import { validateLocale } from './validate_locale';
 
-export const createInitialState = ({ apiKey, selectedLocale}) => {
-  // trying to inject original script with passed parameters
+export const localeRu = {
+  langFail: 'Need to choose supported localization https://tech.yandex.com/maps/doc/intro/concepts/localization-docpage/',
+  zoomIn: 'Приблизить',
+  zoomOut: 'Отдалить',
+  geolocation: 'Мое местопложение'
+};
 
+export const localeEn = {
+  langFail: 'Need to choose supported localization https://tech.yandex.com/maps/doc/intro/concepts/localization-docpage/',
+  zoomIn: 'Zoom in',
+  zoomOut: 'Zoom Out',
+  geolocation: 'Use my location'
+};
+
+export const createInitialState = ({ apiKey, selectedLocale }) => {
+  // trying to inject original script with passed parameters
   let locale;
 
   try {
-    const localeRu = {
-      langFail: 'Need to choose supported localization https://tech.yandex.com/maps/doc/intro/concepts/localization-docpage/',
-      zoomIn: 'Приблизить',
-      zoomOut: 'Отдалить',
-      geolocation: 'Мое местопложение'
-    };
-
-    const localeEn = {
-      langFail: 'Need to choose supported localization https://tech.yandex.com/maps/doc/intro/concepts/localization-docpage/',
-      zoomIn: 'Zoom in',
-      zoomOut: 'Zoom Out',
-      geolocation: 'Use my location'
-    };
-
-    const scripts = document.getElementsByTagName('script');
-    let src = scripts[scripts.length-1].src.split('?')[1];
-    src = src === undefined ? [] : src.split('&');
-    
-    const query = {};
-    src.forEach(function(item, i, arr) {
-      let sStr = item.split('=');
-      query[sStr[0]] = sStr[1];
-    });
-
     if (!selectedLocale || !validateLocale(selectedLocale)) {
       locale = localeEn;
       throw new SyntaxError(locale.langFail);
     };
 
     const script = document.createElement('script');
+    script.id = 'api-maps.yandex';
     script.setAttribute('type', 'text/javascript');
     script.setAttribute('src', `https://api-maps.yandex.ru/2.1/?lang=${selectedLocale}&apikey=${apiKey}`);
     document.getElementsByTagName('head').item(0).appendChild(script);
