2c1079f3ef1730ce394594c477fbf761070198ee
makstakeda <mesketches@gmail.com>
2020-08-09 11:24:04 +0100

    initSmMoveCenter - tested

--- src/modules/init_sm_move_center.js
+++ src/modules/init_sm_move_center.js
@@ -1,19 +1,21 @@
 export const initSmMoveCenter = map => {
-  const movelinks = document.querySelectorAll('a[sm-move]');
+  const movelinks = document.querySelectorAll('[sm-move]');
 
   for (let i = 0; i < movelinks.length; i++) {
-    movelinks[i].addEventListener('click', function(event) {
+    movelinks[i].addEventListener('click', event => {
       event.stopPropagation();
       event.preventDefault();
       const element = event.target;
       const coordinates = element.getAttribute('sm-move').split(',');
       coordinates[0] = Number(coordinates[0]);
       coordinates[1] = Number(coordinates[1]);
+
       if (isNaN(coordinates[0]) || isNaN(coordinates[1])) {
         return console.error(`"${element.getAttribute('sm-move')}" are not correct params at "sm-move" attribute`);
       };
+
       map.setZoom(12);
       map.panTo(coordinates);
     });
   };
-}
+};
